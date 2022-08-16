2c3650e3403597179a2709faa8840fa9aa2268ad
vadimcimbalyk <vadymonua@gmail.com>
2022-08-16 20:01:55 +0300

    add app.vue

--- src/App.vue
+++ src/App.vue
@@ -1,26 +1,53 @@
 <template>
-  <img alt="Vue logo" src="./assets/logo.png">
-  <HelloWorld msg="Welcome to Your Vue.js App"/>
+  <div>
+    <header>
+      <div class="select__language-container">
+        <div class="chose__language"> chose language</div>
+      </div>
+      <div class="nav__container">
+        <nav class="nav_bar">
+          <ul>
+            <li>
+              <a href="#">where?</a>
+            </li>
+            <li>
+              <a href="#">what?</a>
+            </li>
+            <li>
+              <a href="#">who?</a>
+            </li>
+          </ul>
+        </nav>
+      </div>
+
+    </header>
+    <main>
+      <div class="container">
+        <div class="text__content">
+          <h1>
+            FULL-CYCLE
+            EVENT AGENCY
+          </h1>
+        </div>
+      </div>
+    </main>
+
+
+  </div>
 </template>
 
 <script>
-import HelloWorld from './components/HelloWorld.vue'
 
 export default {
   name: 'App',
   components: {
-    HelloWorld
+
   }
 }
 </script>
 
 <style>
 #app {
-  font-family: Avenir, Helvetica, Arial, sans-serif;
-  -webkit-font-smoothing: antialiased;
-  -moz-osx-font-smoothing: grayscale;
-  text-align: center;
-  color: #2c3e50;
-  margin-top: 60px;
+
 }
 </style>
