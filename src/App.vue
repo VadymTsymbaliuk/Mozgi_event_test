ae6000ed08f0a33a5503aa9f8581b3897b0bb0a4
vadimcimbalyk <vadymonua@gmail.com>
2022-08-17 13:06:12 +0300

    html part of the page is complete

--- src/App.vue
+++ src/App.vue
@@ -1,22 +1,28 @@
 <template>
-  <div>
+  <div class="body">
     <header>
       <div class="select__language-container">
-        <div class="chose__language"> chose language</div>
+        <div class="chose__language">👅</div>
+      </div>
+      <div class="logo__container">
+        <img src="../src/assets/img/logo.svg" alt="logotype" class="logo">
       </div>
       <div class="nav__container">
         <nav class="nav_bar">
-          <ul>
-            <li>
+          <ul class="navbar-nav">
+            <li class="navbar-item">
               <a href="#">where?</a>
             </li>
-            <li>
+            <li class="navbar-item">
               <a href="#">what?</a>
             </li>
-            <li>
+            <li class="navbar-item">
               <a href="#">who?</a>
             </li>
           </ul>
+          <button class="toggle__menu toggle__animation">
+            <span></span>
+          </button>
         </nav>
       </div>
 
@@ -25,9 +31,21 @@
       <div class="container">
         <div class="text__content">
           <h1>
-            FULL-CYCLE
+            FULL-CYCLE <br/>
             EVENT AGENCY
           </h1>
+
+          <div class="tape__container">
+            <div class="top__tape">
+              <span>FULL-CYCLE EVENT AGENCY FULL-CYCLE EVENT AGENCY</span>
+            </div>
+            <div class="bottom__tape">
+              <span>FULL-CYCLE EVENT AGENCY FULL-CYCLE EVENT AGENCY</span>
+            </div>
+          </div>
+          <div class="show-reel__container">
+            <img src="../src/assets/img/cirkle.svg" alt="circle">
+          </div>
         </div>
       </div>
     </main>
@@ -40,14 +58,10 @@
 
 export default {
   name: 'App',
-  components: {
 
-  }
 }
 </script>
 
 <style>
-#app {
 
-}
 </style>
