9967bcbdf20e79485a7787bea135f9831b49c07c
vadimcimbalyk <vadymonua@gmail.com>
2022-08-19 11:28:38 +0300

    fix styles

---
+++ src/components/VNavigation.vue
@@ -0,0 +1,31 @@
+<template>
+  <div class="nav__container">
+    <nav class="navbar">
+      <ul class="navbar-nav">
+        <li class="navbar-item">
+          <router-link to="/where">where?</router-link>
+        </li>
+        <li class="navbar-item">
+          <router-link to="/who">who?</router-link>
+        </li>
+        <li class="navbar-item">
+          <router-link to="/what">what?</router-link>
+        </li>
+      </ul>
+      <button class="toggle__menu toggle__animation" @click="openToggleMenu" />
+    </nav>
+  </div>
+</template>
+
+<script>
+export default {
+  name: "VNavigation",
+  props:{
+    openToggleMenu: Function
+  }
+}
+</script>
+
+<style scoped>
+
+</style>
